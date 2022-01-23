47f90462a8ccc871d8289bec89d3c39b8818b302
Veronica Strakhov <92378709+veronicastrakhov@users.noreply.github.com>
2022-01-22 22:19:09 -0500

    Update Battleships.py

--- Battleships.py
+++ Battleships.py
@@ -5,10 +5,22 @@
 # Controls: Use mouse to move left and right
 
 # CREDITS:
-# Art and code created by me, Veronica Strakhov. Contact me at veronicastrakhov@gmail.com
+# Art and code created by me, Veronica Strakhov. Contact me @veronicastrakhov on GitHub
 # Adventure Begins by Bart Kelsy on opengameart.org
 # Bomb Explosion 8bit SFX by Luke.RUSTLTD on opengameart.org
 # Coin01 SFX by crazyduckgames on opengameart.org
+
+# Copyright (c) 2022 Veronica Strakhov. All rights reserved.
+
+# Redistribution and use in source and binary forms are permitted provided that 
+# the above copyright notice and this paragraph are duplicated in all such forms and that any 
+# documentation, advertising materials, and other materials related to such distribution and use 
+# acknowledge that the software was developed by Veronica Strakhov. 
+# The name of the Veronica Strakhov may not be used to endorse or promote products
+# derived from this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED AS IS AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES, 
+# INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 
 import pygame, os, sys, random
 from pygame.locals import *
